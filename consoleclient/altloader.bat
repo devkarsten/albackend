8d01847833475f23e5a89054a99bd5a69940fef9
Karsten <contactkaarsteen@gmail.com>
2023-10-14 18:27:51 +0200

    Added consoleclient folder

---
+++ consoleclient/altloader.bat
@@ -0,0 +1,11 @@
+start /min MinecraftClient.exe mc1@altloader.com password
+timeout 2
+
+start /min MinecraftClient.exe mc2@altloader.com password
+timeout 2
+
+start /min MinecraftClient.exe mc3@altloader.com password
+timeout 2
+
+start /min MinecraftClient.exe mc4@altloader.com password
+timeout 2
